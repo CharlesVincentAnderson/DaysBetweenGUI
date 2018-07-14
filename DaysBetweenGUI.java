c3b26208f6f889ea93788463c6731fc37b77fff6
Charles Anderson <CharlesVincentAnderson@gmail.com>
2018-07-13 21:34:01 -0400

    Update DaysBetweenGUI.java

--- DaysBetweenGUI.java
+++ DaysBetweenGUI.java
@@ -1,8 +1,7 @@
-/***************************************************************************************************************
- * This program creates a menu where "days between" can be selected and the code can be expanded at a later date.
- * Once the days between button has been pressed a new window is created where the user can enter two dates 
- * and find out how many days are in between.
- ***************************************************************************************************************/
+/*****************************************************************************************************************
+ *The first program is a Gui that starts with a button which, when pushed, opens a second window. 
+ *The second window allows the user to input two dates then calculates and displays the number of days in between.
+ *****************************************************************************************************************/
 
 
 import javafx.application.Application;
